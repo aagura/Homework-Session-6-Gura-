d56815e88c055786499f2a95277b6305b9aed19b
Alex Gura <aagura@yandex.ru>
2023-02-13 20:25:29 +0300

    Написал код домашки к семинару 6

---
+++ HT1/Program.cs
@@ -0,0 +1,61 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//0, 7, 8, -2, -2 -> 2
+
+//1, -7, 567, 89, 223-> 3
+
+int GetNumber (string message)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.WriteLine (message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ошибка ввода, вы ввели не число. Повторите ввод:");
+        }
+    }
+    return result;
+}
+
+int [] InputArray(int Lenght)
+{
+    int[] array = new int [Lenght];
+   
+    for (int i = 0; i < array.Length; i++)
+    {
+        
+        array[i] = GetNumber($"Введите элемент массива {i}");
+            }
+return array;
+}
+
+void PrintArray (int [] array)
+{
+    for (int i = 0; i < array.Length-1; i++)
+    {
+       Console.Write($"{array[i]}, "); 
+    }
+    Console.Write(array[array.Length-1]);
+    Console.WriteLine();
+}
+
+
+int CountPos (int [] arr)
+{
+    int result= 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i]>0) result++;
+    }
+return result;
+}
+
+int lengthArray = GetNumber("Введите количество элементов массива: ");
+int [] arrayM = InputArray (lengthArray) ;
+PrintArray (arrayM);
+Console.WriteLine ($"Количество положительных элементов {CountPos (arrayM)}");
---
+++ HT2/Program.cs
@@ -0,0 +1,45 @@
+﻿//Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double GetNumber (string message)
+{
+    double result = 0;
+    while (true)
+    {
+        Console.WriteLine (message);
+        if (double.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ошибка ввода, вы ввели не число. Повторите ввод:");
+        }
+    }
+    return result;
+}
+
+double CalcX (double k1, double b1, double k2, double b2)
+{
+     double x= (b2-b1)/(k1-k2); 
+   return x;
+}
+double k1 =GetNumber ("Введите k1:");
+double b1 =GetNumber ("Введите b1:");
+double k2 =GetNumber ("Введите k2:");
+double b2 =GetNumber ("Введите b2:");
+if (k1!=k2)
+{
+double x =CalcX (k1,b1,k2,b2);
+double y = k1*x+b1;
+Console.WriteLine ($"Точка пересечения [{x} ; {y}]");
+}
+else
+{
+Console.WriteLine ("Прямые не пересекаются");
+}
+
+
+
